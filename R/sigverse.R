21a8ac15bef8bf306fec5ed73d3abccb263f474a
Sam El-Kamand <sam.elkamand@gmail.com>
2024-10-12 08:44:49 +1100

    fix: avoid R CMD Note about unused imports
    Resolves issue #9

---
+++ R/sigverse.R
@@ -0,0 +1,11 @@
+# Suppress R CMD check note
+# Namespace in Imports field not imported from: PKG
+#   All declared Imports should be used.
+ignore_unused_imports <- function() {
+  sigshared::example_signature
+  sigsim::sig_simulate_catalogues_from_signatures
+  sigstash::sig_available
+  sigstats::sig_cosine_similarity
+  sigstory::sigstory
+  sigvis::sig_visualise
+}
